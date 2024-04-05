9955a74a8ead1aaaf36861e941771e1eca156834
Marina Bagrovets <marina.bagrovets@LeverX.local>
2024-04-05 13:29:08 +0300

    polymorphism2

--- src/main/java/Lecture6HT/Interfaces/Dog.java
+++ src/main/java/Lecture6HT/Interfaces/Dog.java
@@ -1,2 +1,43 @@
-package Lecture6HT.Interfaces;public class Dog {
+package Lecture6HT.Interfaces;
+
+public class Dog implements Voice, Swimming, Movement{
+    @Override
+    public void voice(){
+        System.out.println("Bark!");
+    }
+
+    @Override
+    public void swimming() {
+        System.out.println("I can swim");
+    }
+
+    @Override
+    public void jump() {
+        System.out.println("I can jump");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("I can run");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("I can walk");
+    }
+
+    @Override
+    public void climb() {
+        System.out.println("I climb very bad");
+    }
+
+    @Override
+    public void smile() {
+        System.out.println("My smile is funny");
+    }
+
+    @Override
+    public void dive() {
+        System.out.println("Sometimes I dive");
+    }
 }
